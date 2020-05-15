89a7d4a0ff3ebc5b4709438f22537e515f6b3b05
fadeomar <fadeomar2015@gmail.com>
2020-05-15 18:04:39 +0300

    add onBlur function to the inputs

--- Forms/AnimatedLoginForm/app.js
+++ Forms/AnimatedLoginForm/app.js
@@ -1,13 +1,21 @@
 const inputs = document.querySelectorAll('.input');
 
 
-function asd(){
+function onFoucus(){
   let parent = this.parentNode.parentNode;
   parent.classList.add('focus');
 }
 
+function onBlur(){
+  let parent = this.parentNode.parentNode;
+  if(this.value === "") {
+    parent.classList.remove('focus')
+  }
+
+}
 // the arrow function dosen't work this not defind
 
 inputs.forEach(input => {
-  input.addEventListener('focus', asd)
+  input.addEventListener('focus', onFoucus);
+  input.addEventListener('blur', onBlur);
 })
