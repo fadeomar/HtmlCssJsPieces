82ec83fe88fbc0452da24ae4c5b2d7885d1cb4a0
fadeomar <fadeomar2015@gmail.com>
2020-05-23 09:35:09 +0300

    add script to change the classes

---
+++ flexible highlight Navigation/app.js
@@ -0,0 +1,57 @@
+
+const icons = document.querySelectorAll('[data-index]');
+
+let previous = -1;
+
+const movement = (elemet) => {
+  elemet.classList.add("initialised");
+  let index = elemet.getAttribute("data-index")
+  let navtab = document.querySelector('nav');
+  navtab.setAttribute("data-selected", index)
+  console.log(navtab)
+  if(previous == -1) {
+    document.querySelector('[data-index="2"]').classList.add("initialised");
+  }
+
+  if(previous == 1 && index == 3 || previous == 3 && index == 1) { 
+    //If going from one side to the other and middle needs to be hidden
+    document.querySelector('[data-index="2"]').classList.remove("initialised");
+  }
+
+  setTimeout(function(){ 
+    //Because apparently this is the only way it will work
+    // navtab.find('.icon[data-index="2"]').addClass("initialised");
+    document.querySelector('[data-index="2"]').classList.add("initialised");
+    //Same animation as the other so they line up
+  });
+
+  previous = index;
+
+  setTimeout(function(){
+    navtab.classList.remove("moving")
+    navtab.classList.remove("hidemiddle")
+}, 750);
+}
+
+
+icons.forEach((elm) => {
+  elm.addEventListener('click', (e => movement(elm)))
+})
+
+
+// $(".icon[data-index]").click(function(){
+//     $(this).addClass("initialised");
+//     let index = $(this).attr("data-index"); //
+//     let navtab = $(this).closest("nav.tab").addClass("moving").attr("data-selected", index); //
+//     if(previous == -1) navtab.find('.icon[data-index="2"]').addClass("initialised") //
+//     if(previous == 1 && index == 3 || previous == 3 && index == 1) { //If going from one side to the other and middle needs to be hidden
+//         navtab.find('.icon[data-index="2"]').removeClass("initialised"); //
+//         setTimeout(function(){ //Because apparently this is the only way it will work
+//             navtab.find('.icon[data-index="2"]').addClass("initialised"); //Same animation as the other so they line up
+//         });
+//     }
+//     previous = index;
+//     setTimeout(function(){
+//         navtab.removeClass("moving").removeClass("hidemiddle");
+//     }, 750);
+// }); 
