1c719f94fa4551be3ce28212eb581c6b53d13f3c
fadeomar <fadeomar2015@gmail.com>
2020-05-26 21:25:07 +0300

    change the image according to the selected color

--- AnimatedCardModel/app.js
+++ AnimatedCardModel/app.js
@@ -1,17 +1,22 @@
 const sizes = document.querySelectorAll('.size');
 const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe');
 
 const changeColor = (elm) => {
-  let primary = elm.getAttribute('primary'); 
-  let color = elm.getAttribute('color')
-  console.log(primary)
+  const primary = elm.getAttribute('primary'); 
+  const color = elm.getAttribute('color');
+  const shoe = document.querySelector(`.shoe[color=${color}]`)
+  console.log({primary, color, shoe})
   sizes.forEach(size => size.classList.remove('active'))
-  elm.classList.add('active')
+  elm.classList.add('active');
+
+  shoes.forEach(shoe => shoe.classList.remove('show'));
+  shoe.classList.add('show');
 }
 
 const changeSize = (elm) => {
   colors.forEach(color => color.classList.remove('active'))
-  elm.classList.add('active')
+  elm.classList.add('active');
 }
 
 sizes.forEach(size => {
