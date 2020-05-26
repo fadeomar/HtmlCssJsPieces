330ed255ece8c9ef78defdcd8aef8c7aa3024cd9
fadeomar <fadeomar2015@gmail.com>
2020-05-26 21:10:08 +0300

    edit the primary attribute to the color not for gradient

--- AnimatedCardModel/app.js
+++ AnimatedCardModel/app.js
@@ -1,10 +1,22 @@
 const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+
+const changeColor = (elm) => {
+  let primary = elm.getAttribute('primary'); 
+  console.log(elm)
+  sizes.forEach(size => size.classList.remove('active'))
+  elm.classList.add('active')
+}
 
 const changeSize = (elm) => {
-  sizes.forEach(size => size.classList.remove('active'))
+  colors.forEach(color => color.classList.remove('active'))
   elm.classList.add('active')
 }
 
 sizes.forEach(size => {
   size.addEventListener('click', () => changeSize(size))
 })
+
+colors.forEach(color => {
+  color.addEventListener('click', () => changeColor(color))
+});
